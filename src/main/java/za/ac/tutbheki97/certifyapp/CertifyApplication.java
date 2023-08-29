f2af4b70fdaad1aeabc98097a4fa18199a8f477d
bheki97 <bfmokoena7@gmail.com>
2023-08-29 11:49:23 +0200

    initial Commit

---
+++ src/main/java/za/ac/tutbheki97/certifyapp/CertifyApplication.java
@@ -0,0 +1,15 @@
+package za.ac.tutbheki97.certifyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertifyApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CertifyApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/za/ac/tutbheki97/certifyapp/CertifyApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.tutbheki97.certifyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
